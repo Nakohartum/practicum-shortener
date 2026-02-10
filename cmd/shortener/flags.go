09825a232e8acf3be550acb2a51750f89042a8ba
Nakohartum <artemofgta@gmail.com>
2026-02-10 17:25:55 +0500

    fix: parsing flags

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -12,4 +12,5 @@ var serverFlags struct{
 func readFlags() {
 	flag.StringVar(&serverFlags.address, "a", "localhost:8080", "address of server")
 	flag.StringVar(&serverFlags.shortenBaseAddress, "b", "localhost:8080", "shorten address base")
+	flag.Parse()
 }
